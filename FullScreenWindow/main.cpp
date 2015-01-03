62a39daeccc5d389380ccb7960175807ed101b8a
Miow <abrioy@gmail.com>
2015-01-03 05:16:35 +0100

    Added logging capabilities and moving/resizing of windows with win32 api

--- FullScreenWindow/main.cpp
+++ FullScreenWindow/main.cpp
@@ -2,16 +2,17 @@
 #include <QtWidgets/QApplication>
 
 #include "WindowHandling.h"
+#include "log.h"
 
 
 int main(int argc, char *argv[])
 {
-
+	initLog();
 
 	Wrapper::WindowHandle* wh = new Wrapper::WindowHandle();
 	Wrapper::WindowHandling::getWindowByName(L"VLC media player", wh);
-	ShowWindow(wh->hWnd, SW_SHOW);
-
+	
+	Wrapper::WindowHandling::moveAndResizeWindow(*wh, 100, 300, 500, 200);
 
 
 
